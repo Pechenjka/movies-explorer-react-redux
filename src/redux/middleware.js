4d316ea5b551b83d88e9f0fa3c29a08803b27ca9
Petr Lobachev <petya.lobachev@yandex.ru>
2021-05-18 12:00:59 +0300

    fix: удалить не используемый файл middleware

--- src/redux/middleware.js
+++
@@ -1,9 +0,0 @@
-//
-// const handleSignOut = () => {
-//   const jwt = localStorage.getItem("jwt");
-//   if (jwt) {
-//     localStorage.removeItem("jwt");
-//     localStorage.removeItem("storageMovies");
-//     setLoggedIn(false);
-//   }
-// };
